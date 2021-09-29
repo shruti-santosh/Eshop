2b7665502989eee4a8bbf0cccdb9cdeb135bdefa
shruti-santosh <shrutisantosh@yahoo.com>
2021-09-29 18:52:22 +0530

    made react app with basic front end

--- src/App.js
+++ src/App.js
@@ -2,7 +2,84 @@ import React from 'react';
 
 function App(){
   return  (
+    
+    <div className="grid-container">
+        <header className="row">
+            <div>
+                <a className="brand" href="index.html">Eshop</a>
+            </div>
+            <div>
+                <a href= "cart.html">Cart</a>
+                <a href = "signin.html" > Signin</a>
+            </div>
+        </header>
+        <main>
+            <div className="row center">
+                <div className="cards">
+                    <a href="product.html">
+                        <img className="medium" src="./images/istockphoto-1005339436-612x612.jpg" alt="potato"/>
+                    </a>
+                    <div className="card-body">
+                        <a href="product.html">
+                            <h2>Potato</h2>
+                        </a>
+                        <div className="price">
+                            Rs 30
+                        </div>
+                    </div>
 
+                </div>
+            
+            
+                <div className="cards">
+                    <a href="product.html">
+                        <img className="medium" src="./images/istockphoto-173242750-612x612.jpg" alt="potato"/>
+                    </a>
+                    <div className="card-body">
+                        <a href="product.html">
+                            <h2>Banana</h2>
+                        </a>
+                        <div className="price">
+                            Rs 50
+                        </div>
+                    </div>
+
+                </div>
+                <div className="cards">
+                    <a href="product.html">
+                        <img className="medium" src="./images/Organic-Drumstick_large_4080ea56-52a3-4647-9d3d-98c2d72afed2.jpg" alt="potato"/>
+                    </a>
+                    <div className="card-body">
+                        <a href="product.html">
+                            <h2>Drumsticks</h2>
+                        </a>
+                        <div className="price">
+                            Rs 20
+                        </div>
+                    </div>
+
+                </div>
+                <div className="cards">
+                    <a href="product.html">
+                        <img className="medium" src="./images/orange.jpg" alt="potato"/>
+                    </a>
+                    <div className="card-body">
+                        <a href="product.html">
+                            <h2>Orange</h2>
+                        </a>
+                        <div className="price">
+                            Rs 25
+                        </div>
+                    </div>
+                </div>
+                </div>
+            
+        </main>
+        <footer className="row center">
+            EShop welcomes you to buy our 4 products
+        </footer>
+
+    </div>
   );
 }
 export default App;
---
+++ src/data.js
@@ -0,0 +1,36 @@
+const data = {
+    products: [
+        {
+            _id : '1',
+            name: 'Potato',
+            price: 30,
+            category: 'Vegetables',
+            vendor: 'Himachal Pvt Ltd',
+            available: 1,
+        },
+        {
+            _id : '2',
+            name: 'Banana',
+            price: 50,
+            category: 'Fruits',
+            vendor: 'Organic Farms',
+            available: 1,
+        },
+        {
+            _id : '3',
+            name: 'Drumsticks',
+            price: 20,
+            category: 'Vegetables',
+            vendor: 'Mallikarjuna Farms',
+            available: 0
+        },
+        {
+            _id : '4',
+            name: 'Orange',
+            price: 25,
+            category: 'Fruit',
+            vendor: 'Nagpur',
+            available: 1
+        },
+    ]
+}
